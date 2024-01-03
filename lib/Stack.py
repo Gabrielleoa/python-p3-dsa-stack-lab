9683b42d0f6888253f440c51b6b8288729e03315
Gabrielle <gabrielle.odhiambo@student.moringaschool.com>
2024-01-03 10:29:37 +0300

    completed assignment

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,13 +1,17 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items= [1,2,3,4,5]
+        
 
-    def isEmpty(self):
-        pass
+
+
+    def is_empty(self):
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        self.items.append(item)
+       
 
     def pop(self):
         pass
@@ -16,10 +20,15 @@ class Stack:
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
-    def full(self):
-        pass
-
+    def full(self, item):
+        if len(self.items) < self.capacity:
+             self.items.append(item)
+        else:
+            print('cannot push')
     def search(self, target):
-        pass
+         for index, item in enumerate(self.items[::-1], 1):
+            if item == target:
+                return index
+         return -1
